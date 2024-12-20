d41364250a87f3dd78a9dc778daba1029367116a
0tr1cala-v <marenkov2015@mail.ru>
2024-12-20 16:30:54 +0300

    Add files via upload

--- django_module_19/task1/models.py
+++ django_module_19/task1/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
 class Buyer(models.Model):
-    name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
-    age = models.PositiveIntegerField()
+    name = models.CharField(max_length=100, unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    age = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
--- django_module_19/task1/views.py
+++ django_module_19/task1/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Buyer, Game
+from django.http import HttpResponse
 
 # Create your views here.
+def buy_game(request, game_id):
+    """
+    Представление для покупки игры.
+    Проверяет возраст покупателя и достаточность баланса.
+    """
+    game = get_object_or_404(Game, pk=game_id)
+    buyer = get_object_or_404(Buyer, name=request.user.username) # Предполагается аутентификация пользователя
+
+    if request.method == 'POST':
+        if buyer.age >= 18 or not game.age_limited:  # Проверка возраста
+            if buyer.balance >= game.cost: # Проверка баланса
+                buyer.balance -= game.cost
+                buyer.save()
+                game.buyers.add(buyer)
+                return HttpResponse("Игра успешно куплена!")  # Можно сделать более красивое сообщение
+            else:
+                return HttpResponse("Недостаточно средств на балансе.")
+        else:
+            return HttpResponse("Вы слишком молоды для этой игры.")
+    else:
+        context = {'game': game, 'buyer': buyer}  #  Передаем данные в шаблон
+        return render(request, 'buy_game.html', context)
